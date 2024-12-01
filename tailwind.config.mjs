d00bde66791a84279ece5a49c9cc873bd5549653
mnibor <marcelorobin@gmail.com>
2024-12-01 12:51:03 -0300

    definicion de tipografias

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Nunito', 'system-ui', 'sans-serif'],
+				inter: ['Inter', 'sans-serif'],
+				mono: ['Fira Code', 'monospace'],
+			},
+			fontSize: {
+				xs: '.75rem',
+				sm: '.875rem',
+				base: '1rem',
+				lg: '1.125rem',
+				xl: '1.25rem',
+				'2xl': '1.5rem',
+				'3xl': '1.875rem',
+				'4xl': '2.25rem',
+				'5xl': '3rem',
+				'6xl': '4rem',
+			},
+		},
 	},
 	plugins: [],
 }
